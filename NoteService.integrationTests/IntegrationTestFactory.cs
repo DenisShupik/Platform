f294e571a2a701ad8f36765db4c89076024b2d5c
DenisAloner <denisaloner@gmail.com>
2024-10-20 15:13:48 +0300

    1

---
+++ NoteService.integrationTests/IntegrationTestFactory.cs
@@ -0,0 +1,41 @@
+﻿using DotNet.Testcontainers.Builders;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Testcontainers.PostgreSql;
+
+namespace NoteService.integrationTests;
+
+[CollectionDefinition(nameof(IntegrationTestFactoryCollection))]
+public class IntegrationTestFactoryCollection : ICollectionFixture<IntegrationTestFactory>
+{
+}
+
+public sealed class IntegrationTestFactory : WebApplicationFactory<Program>, IAsyncLifetime
+{
+    private readonly PostgreSqlContainer _container = new PostgreSqlBuilder()
+        .WithImage("postgres:latest")
+        .WithDatabase("platform_db_tests")
+        .WithUsername("admin")
+        .WithPassword("12345678")
+        .WithWaitStrategy(Wait.ForUnixContainer().UntilCommandIsCompleted("pg_isready"))
+        .WithCleanUp(true)
+        .Build();
+
+    public async Task InitializeAsync()
+    {
+        await _container.StartAsync();
+    }
+
+    public new async Task DisposeAsync()
+    {
+        await _container.DisposeAsync();
+    }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.UseSetting(
+            "NoteServiceOptions:ConnectionString",
+            "Host=localhost;Port=5432;Database=platform_db_tests;Username=admin;Password=12345678;Pooling=true;Minimum Pool Size=0;Maximum Pool Size=100;"
+        );
+    }
+}
---
+++ NoteService.integrationTests/NoteServiceTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http.Json;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace NoteService.integrationTests;
+
+[Collection(nameof(IntegrationTestFactoryCollection))]
+public sealed class NoteServiceTests
+{
+    private readonly IntegrationTestFactory _factory;
+
+    public NoteServiceTests(IntegrationTestFactory factory)
+    {
+        _factory = factory;
+    }
+    
+    [Fact]
+    public async Task Get_Information_From_Database_Endpoint()
+    {
+        var client = _factory.CreateClient();
+        var userId = Guid.NewGuid();
+        var actual = await client.GetFromJsonAsync<int>($"api/users/{userId}/notes/count");
+        Assert.IsType<Results<Ok<int>, NotFound>>(actual);
+        Assert.Equal(expected: 10, actual);
+    }
+}
