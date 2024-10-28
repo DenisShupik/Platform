a3e68f62ee3f3d4d601332be1c6a5f0edc68ee31
DenisShupik <maildenisshupik@gmail.com>
2024-10-28 04:55:53 +0300

    1

--- frontend/src/lib/components/CategoryView.svelte
+++ frontend/src/lib/components/CategoryView.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
   import type { Category } from '$lib/types/Category'
+  import { Separator } from '$lib/components/ui/separator'
 
   let { category }: { category: Category } = $props()
 </script>
 
-<div class="w-full">{category.title}</div>
+<div class="w-full flex items-center text-sm">
+  <div>{category.title}</div>
+  <div class="ml-auto flex h-5 items-center space-x-4">
+    <div>еще инфа</div>
+    <Separator orientation="vertical" />
+    <div>еще инфа</div>
+  </div>
+</div>
--- frontend/src/lib/components/SectionView.svelte
+++ frontend/src/lib/components/SectionView.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
   import * as Collapsible from '$lib/components/ui/collapsible'
   import { ChevronUp } from 'lucide-svelte'
-  import { Button, buttonVariants } from '$lib/components/ui/button'
+  import { buttonVariants } from '$lib/components/ui/button'
+  import { Separator } from '$lib/components/ui/separator'
   import CategoryView from './CategoryView.svelte'
   import type { Section } from '$lib/types/Section'
   import CreateCategoryDialog from './CreateCategoryDialog.svelte'
@@ -11,10 +12,10 @@
 </script>
 
 <Collapsible.Root
-  class="w-full bg-card text-card-foreground rounded-lg border shadow-sm space-y-2"
+  class="w-full bg-card text-card-foreground rounded-lg border shadow-sm grid gap-y-2"
   bind:open={isOpen}
 >
-  <div class="bg-red-800 flex items-center px-4">
+  <div class="bg-red-900 flex items-center px-4">
     <h4 class="text-sm font-semibold">{section.title}</h4>
     <div class="ml-auto flex space-x-4">
       <CreateCategoryDialog sectionId={section.sectionId} />
@@ -30,9 +31,14 @@
       </Collapsible.Trigger>
     </div>
   </div>
-  <Collapsible.Content class="space-y-2 p-4 pt-0">
-    {#each section.categories ?? [] as category}
-      <CategoryView {category} />
-    {/each}
-  </Collapsible.Content>
+  {#if section.categories != null && section.categories.length !== 0}
+    <Collapsible.Content class="px-4 pb-2 pt-0">
+      {#each section.categories ?? [] as category, index}
+        <CategoryView {category} />
+        {#if index < (section.categories?.length ?? 0) - 1}
+          <Separator class="my-2" />
+        {/if}
+      {/each}
+    </Collapsible.Content>
+  {/if}
 </Collapsible.Root>
