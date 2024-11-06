755ebb77315bbaa6185d4e97bf140e5aeb3e6b09
DenisShupik <maildenisshupik@gmail.com>
2024-11-06 16:35:06 +0300

    1

---
+++ frontend/src/routes/categories/[categoryId]/+page.svelte
@@ -0,0 +1,102 @@
+<script lang="ts">
+  import { GET } from '$lib/GET'
+  import type { KeysetPage } from '$lib/types/KeysetPage'
+  import type { Topic } from '$lib/types/Topic'
+  import TopicView from '$lib/components/TopicView.svelte'
+  import * as Breadcrumb from '$lib/components/ui/breadcrumb'
+  import type { Category } from '$lib/types/Category'
+  import type { Section } from '$lib/types/Section'
+  import BreadcrumbRouteLink from '$lib/components/ui/route-link/BreadcrumbRouteLink.svelte'
+  import { categoryStore } from '$lib/stores/categoryStore'
+  import { sectionStore } from '$lib/stores/sectionStore'
+  import Paginator from '$lib/components/Paginator.svelte'
+  import { page } from '$app/stores'
+
+  let categoryId: Category['categoryId'] = $derived(
+    parseInt($page.params.categoryId)
+  )
+
+  let topicCount: number | undefined = $state()
+  let topics: KeysetPage<Topic> | undefined = $state()
+  let category = $derived($categoryStore.get(categoryId))
+  let section = $derived(
+    category === undefined ? undefined : $sectionStore.get(category.sectionId)
+  )
+
+  $effect(() => {
+    if (category === undefined) {
+      GET<Category>(`/categories/${categoryId}`).then((v) =>
+        categoryStore.update((e) => {
+          e.set(categoryId, v)
+          return e
+        })
+      )
+    }
+  })
+
+  $effect(() => {
+    if (category !== undefined && section === undefined) {
+      GET<Section>(`/sections/${category.sectionId}`).then((v) =>
+        sectionStore.update((e) => {
+          e.set(category.sectionId, v)
+          return e
+        })
+      )
+    }
+  })
+
+  $effect(() => {
+    if (category !== undefined && topicCount === undefined) {
+      GET<number>(`/categories/${categoryId}/topics/count`).then(
+        (v) => (topicCount = v)
+      )
+    }
+  })
+
+  $effect(() => {
+    if (category !== undefined && topics === undefined) {
+      GET<KeysetPage<Topic>>(`/categories/${categoryId}/topics`).then(
+        (v) => (topics = v)
+      )
+    }
+  })
+</script>
+
+{#if category === undefined}
+  <p>Загрузка тем...</p>
+{:else}
+  <Breadcrumb.Root>
+    <Breadcrumb.List>
+      <Breadcrumb.Item>
+        <BreadcrumbRouteLink link="/" title="Разделы" />
+      </Breadcrumb.Item>
+      <Breadcrumb.Separator />
+      <Breadcrumb.Item>
+        {#if section}
+          <BreadcrumbRouteLink
+            link={`/sections/${section.sectionId}`}
+            title={section.title}
+          />
+        {:else}
+          <div>Получаю данные</div>
+        {/if}
+      </Breadcrumb.Item>
+    </Breadcrumb.List>
+  </Breadcrumb.Root>
+  <h1 class="text-2xl font-bold">{category?.title}</h1>
+  <Paginator count={topicCount} />
+  {#if topics != null}
+    <table class="mt-4 w-full table-fixed border-collapse border">
+      <colgroup>
+        <col class="w-16" />
+        <col />
+        <col class="hidden w-24 md:table-column" />
+        <col class="hidden w-32 md:table-column" />
+        <col class="hidden w-12 md:table-column" />
+      </colgroup>
+      {#each topics.items as topic}
+        <TopicView {topic} />
+      {/each}
+    </table>
+  {/if}
+{/if}
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,26 +1,22 @@
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-import path from 'path';
-import fs from 'fs';
+import { defineConfig } from 'vite'
+import { sveltekit } from '@sveltejs/kit/vite'
+import path from 'path'
+import fs from 'fs'
 
 export default defineConfig({
-    server: {
-        https: {
-            key: fs.readFileSync('../service.key'),
-            cert: fs.readFileSync('../service.crt'),
-        },
-    },
-    plugins: [
-        svelte({
-            /* plugin options */
-        }),
-    ],
-    resolve: {
-        alias: {
-            $lib: path.resolve("./src/lib"),
-        },
-    },
-    build: {
-        target: "esnext"
-    },
-});
+  server: {
+    https: {
+      key: fs.readFileSync('../service.key'),
+      cert: fs.readFileSync('../service.crt')
+    }
+  },
+  plugins: [sveltekit()],
+  resolve: {
+    alias: {
+      $lib: path.resolve('./src/lib')
+    }
+  },
+  build: {
+    target: 'esnext'
+  }
+})
