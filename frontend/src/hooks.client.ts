e92034a5d4dd77aa2cdc6234d428de103a269e83
DenisShupik <maildenisshupik@gmail.com>
2025-04-13 22:59:05 +0300

    1

--- frontend/src/hooks.client.ts
+++ frontend/src/hooks.client.ts
@@ -1,7 +1,7 @@
-import { PUBLIC_API_URL } from '$env/static/public'
+import { PUBLIC_CSR_API_URL } from '$env/static/public'
 import { client } from '$lib/utils/client/client.gen'
 
 client.setConfig({
-	baseUrl: PUBLIC_API_URL,
+	baseUrl: PUBLIC_CSR_API_URL,
 	querySerializer: { array: { explode: false } }
 })
--- frontend/src/routes/+layout.server.ts
+++ frontend/src/routes/+layout.server.ts
@@ -2,6 +2,6 @@ import { env } from '$env/dynamic/public'
 import { client } from '$lib/utils/client/client.gen'
 
 client.setConfig({
-	baseUrl: env.PUBLIC_API_URL,
+	baseUrl: env.PUBLIC_SSR_API_URL,
 	querySerializer: { array: { explode: false } }
 })
