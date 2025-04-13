a89789c0319bb59a9a48513eebdaa6613caa2452
DenisShupik <maildenisshupik@gmail.com>
2025-04-13 21:47:12 +0300

    delete me

--- backend/ApiGateway/Program.cs
+++ backend/ApiGateway/Program.cs
@@ -1,10 +1,13 @@
 using ApiGateway.Extensions;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Linq;
 using Yarp.ReverseProxy.Swagger;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var configuration = builder.Configuration.GetSection("ReverseProxy");
+
+Console.WriteLine($"Section: {SerializeConfiguration(configuration)}");
 
 builder.Services
     .AddReverseProxy()
@@ -36,3 +39,49 @@ app.UseCors("AllowLocalhost");
 app.MapReverseProxy();
 
 await app.RunAsync();
+
+JToken SerializeConfiguration(IConfiguration config)
+{
+    var obj = new JObject();
+    foreach (var child in config.GetChildren())
+    {
+        if (child.Path.EndsWith(":0"))
+        {
+            var arr = new JArray();
+
+            foreach (var arrayChild in config.GetChildren())
+            {
+                arr.Add(SerializeConfiguration(arrayChild));
+            }
+
+            return arr;
+        }
+
+        obj.Add(child.Key, SerializeConfiguration(child));
+    }
+
+    if (obj.HasValues || config is not IConfigurationSection section) return obj;
+    
+    // Allow for json that has been embeded as a string in a single key
+    if (section.Value.StartsWith('{') && section.Value.EndsWith('}'))
+    {
+        obj = JObject.Parse(section.Value);
+        return obj;
+    }
+
+    return ParseJValue(section.Value);
+}
+
+JValue ParseJValue(string value)
+{
+    if (bool.TryParse(value, out var boolean))
+        return new JValue(boolean);
+
+    if (long.TryParse(value, out var integer))
+        return new JValue(integer);
+    
+    if (decimal.TryParse(value, out var real))
+        return new JValue(real);
+    
+    return new JValue(value);
+}
