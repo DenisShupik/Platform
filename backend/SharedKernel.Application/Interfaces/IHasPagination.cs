c95752cd340811fdd5b47aa17d4d53a2d485e256
DenisShupik <maildenisshupik@gmail.com>
2025-08-25 17:44:46 +0300

    1

--- backend/SharedKernel.Application/Interfaces/IHasPagination.cs
+++ backend/SharedKernel.Application/Interfaces/IHasPagination.cs
@@ -2,7 +2,7 @@ using SharedKernel.Application.ValueObjects;
 
 namespace SharedKernel.Application.Interfaces;
 
-public interface IPagedQuery<T>
+public interface IHasPagination<T>
     where T : struct, IPaginationLimit, IVogen<T, int>
 {
     public PaginationOffset? Offset { get; }
